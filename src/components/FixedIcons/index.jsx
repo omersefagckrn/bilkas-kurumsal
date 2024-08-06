d9566f0cbedfeeed1316e0ababe35988182845a8
Ömer Sefa Güçkıran <omergckrnx@gmail.com>
2024-08-06 21:09:57 +0300

    bazı düzeltmeler

--- src/components/FixedIcons/index.jsx
+++ src/components/FixedIcons/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
-import { FaArrowUp, FaWhatsapp, FaCookieBite } from 'react-icons/fa';
+import { FaArrowUp, FaWhatsapp, FaCookieBite, FaPhone } from 'react-icons/fa';
 import Modal from '../Modal';
+import { KURUMSAL_NUMARA } from '../../constants';
 
 const FixedIcons = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -29,17 +30,15 @@ const FixedIcons = () => {
 	return (
 		<div className='fixed bottom-4 right-4 flex flex-col items-center space-y-2'>
 			{isVisible && (
-				<button onClick={scrollToTop} className='p-3 bg-black text-white rounded-full shadow-md hover:bg-primary-dark focus:outline-none'>
+				<button onClick={scrollToTop} className='p-3 bg-black text-white rounded-full shadow-md'>
 					<FaArrowUp size={24} />
 				</button>
 			)}
-			<a
-				href='https://wa.me/YOUR_PHONE_NUMBER'
-				className='p-3 bg-primary text-white rounded-full shadow-md hover:bg-green-600 focus:outline-none'
-				target='_blank'
-				rel='noopener noreferrer'
-			>
+			<a href='https://wa.me/YOUR_PHONE_NUMBER' className='p-3 text-white rounded-full shadow-md bg-green-600' target='_blank' rel='noopener noreferrer'>
 				<FaWhatsapp className='text-white' size={24} />
+			</a>
+			<a href={`tel:${KURUMSAL_NUMARA}`} className='p-3 bg-green-600 text-white rounded-full shadow-md phone-icon'>
+				<FaPhone className='text-white' size={24} />
 			</a>
 			<div className='p-3 bg-primary text-white rounded-full cursor-pointer flex items-center justify-center' onClick={handleOpenModal}>
 				<FaCookieBite size={24} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './components/Home';
+import BlogDetail from './components/Solutions/SolutionsDetail';
 
 import './index.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />
+	},
+	{
+		path: '/blog/:title',
+		element: <BlogDetail />
+	}
+]);
+
+createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
