2f47a36f296bfeb080fadae449fc5136c0ebe4b9
Ömer Sefa Güçkıran <omergckrnx@gmail.com>
2024-09-04 13:00:18 +0300

    serve

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				}
+			}
+		},
+		chunkSizeWarningLimit: 2000
+	}
 });
